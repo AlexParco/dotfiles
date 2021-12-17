bab89d41c2da729a8509c17a9a645fc5f95cdc73
AlexParco <alexparco16@gmail.com>
2021-12-16 22:57:57 -0500

    polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
-# Wait until the processes have been shut down
+# Launch bar1 and bar2
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar -rq main &
-#polybar -rq left &
-#polybar -rq right &
-#polybar -rq center &
 
-echo "Polybar launched..."
+echo "Bars launched..."
